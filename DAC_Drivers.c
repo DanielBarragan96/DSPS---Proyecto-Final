b50e64e28d56c25ecfa86b42426c3572acebff43
Cornucopia27 <ie703121@iteso.mx>
2017-11-23 18:22:48 -0600

    DAC_drivers early form

---
+++ DAC_Drivers.c
@@ -0,0 +1,20 @@
+/*
+ * DAC_Drivers.c
+ *
+ *  Created on: 01/10/2017
+ *      Author: Paco
+ */
+#include "DataTypeDefinitions.h"
+#include "MK64F12.h"
+
+
+
+
+ void init_DAC0(void){
+     SIM->SCGC2 = 0x1000;
+     DAC0->C0 = 0xC0;
+ }
+ void DAC0_output(uint16 x){
+	 DAC0->DAT[0].DATL = (uint8) (0xFF & x);
+	 DAC0->DAT[0].DATH = (uint8) (x >> 8);
+ }
---
+++ DAC_Drivers.h
@@ -0,0 +1,18 @@
+/*
+ * DAC_Drivers.h
+ *
+ *  Created on: 01/10/2017
+ *      Author: Paco
+ */
+
+#ifndef DAC_DRIVERS_H_
+#define DAC_DRIVERS_H_
+
+
+
+
+
+void init_DAC0(void);
+void DAC0_output(uint16 x);
+
+#endif /* DAC_DRIVERS_H_ */
