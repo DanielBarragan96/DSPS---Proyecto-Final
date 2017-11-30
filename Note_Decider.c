f17676921b0dbe4aa96da7bb517ad68b8436fec7
Cornucopia27 <ie703121@iteso.mx>
2017-11-29 19:35:31 -0600

    Fixes on notes no more than 3 in a row

--- Note_Decider.c
+++ Note_Decider.c
@@ -15,7 +15,6 @@
 //TODO lijar pinche tarima/pensar que ponerle arriba.
 //TODO contador para los filtros de soule.
 //TODO contador cambian cada cuanto se mandan las flechas diferentes funciones que cambian ese rate y usar funct poin.
-//TODO minsalida-x is new 0 maxsalida+x is 1.
 
     static ufloat32 Note_Type = 0;
     static ufloat32 Note_Prom = 0;
@@ -27,7 +26,11 @@
     static float VALUES[32000] ={0};
     static uint16 salida = 0;
     static float notemax = 0;
-	static float notemin = .50;
+	static float notemin = .5060;
+	static uint16 Down_counter = 0;
+	static uint16 Left_counter = 0;
+	static uint16 Right_counter = 0;
+	static uint16 Up_counter = 0;
 
 /*s*/
 static uint32 Fs = 0;
@@ -71,7 +74,7 @@ void Music_Processor()
 			if(TRUE == get_FrecuencyFlag())
 			{
 				Note_Type -= VALUES[Index];
-				med = VALUES[Index];
+//				med = VALUES[Index];
 //				if(med > max)
 //				{
 //					max = med;
@@ -90,30 +93,72 @@ void Music_Processor()
 			}
 				if(Index == (Get_Fs()-1))
 				{
-					Note_Prom = ((Note_Type/(Get_Fs()))-.486610)/.005374;
-					if(Note_Prom > notemax)
-					{
-						notemax = Note_Prom;
-					}
-					if((Note_Prom < notemin) && (Note_Prom > 0))
-					{
-						notemin = Note_Prom;
-					}
+					Note_Prom = ((Note_Type/(Get_Fs()))-.502070)/.004945;//-.486610)/.005374;
+//					if(Note_Prom > notemax)
+//					{
+//						notemax = Note_Prom;
+//					}
+//					if((Note_Prom < notemin) && (Note_Prom > 0))
+//					{
+//						notemin = Note_Prom;
+//					}
+
 					if(Note_Prom < ABAJO)
 					{
-						addTile(COLUMN_3);
-						Note_Out = 2;
-
+						Left_counter = 0;
+						Right_counter = 0;
+						Up_counter = 0;
+						if(Down_counter == 4)
+						{
+							addTile(COLUMN_4);
+							Down_counter = 0;
+						}else
+						{
+							addTile(COLUMN_3);
+						}
+						Down_counter++;
 					}else if(Note_Prom < IZQUIERDA && Note_Type > ABAJO)
 					{
-						addTile(COLUMN_1);
-						Note_Out = 0;
+						Down_counter = 0;
+						Right_counter = 0;
+						Up_counter = 0;
+						if(Left_counter == 4)
+						{
+							addTile(COLUMN_2);
+							Left_counter = 0;
+						}else
+						{
+							addTile(COLUMN_1);
+						}
+						Left_counter++;
 					}else if(Note_Prom < DERECHA && Note_Type > IZQUIERDA)
 					{
-						addTile(COLUMN_4);
-						Note_Out = 3;
+						Down_counter = 0;
+						Left_counter = 0;
+						Up_counter = 0;
+						if(Right_counter == 4)
+						{
+							addTile(COLUMN_3);
+							Right_counter = 0;
+						}else
+						{
+							addTile(COLUMN_4);
+						}
+						Right_counter++;
 					}else
+					{
+						Down_counter = 0;
+						Left_counter = 0;
+						Right_counter = 0;
+						if(Up_counter == 4)
+						{
+							addTile(COLUMN_1);
+							Up_counter = 0;
+						}else
+						{
 						addTile(COLUMN_2);
-						Note_Out = 1;
+						}
+						Up_counter++;
+					}
 				}
 }
